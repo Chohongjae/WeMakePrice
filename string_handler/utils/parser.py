c0808f23413471b872739ab55248559f566edc80
chohongjae <bjq913@naver.com>
2020-05-16 15:51:45 +0900

    리팩토링

--- string_handler/utils/parser.py
+++ string_handler/utils/parser.py
@@ -5,19 +5,20 @@ import requests
 
 
 class Parser:
-    def __init__(self, url):
+    def __init__(self, url, condition, count):
         self.url = url
+        self.condition = condition
+        self.count = count
 
     def get_html(self):
         return requests.get(self.url).text
 
-    @staticmethod
-    def distribute_text(text, condition):
+    def distribute_text(self, text):
         stack = []
         alphabet = []
         decimal = []
 
-        if condition == 'exclude_html_tag':
+        if self.condition == 'exclude_html_tag':
             for i in text:
                 if i == '<':
                     stack.append(i)
@@ -39,3 +40,20 @@ class Parser:
     @staticmethod
     def merge_each_items(alphabet, decimal):
         return ''.join([item for items in zip_longest(alphabet, decimal) for item in items if item is not None])
+
+    def divide_text(self, text):
+        length = len(text)
+        if length < self.count:
+            quotient = ''
+            remainder = text
+        elif length == self.count:
+            quotient = text
+            remainder = ''
+        else:
+            quotient = text[:(length // self.count) * self.count]
+            if quotient == text:
+                remainder = ''
+            else:
+                remainder = text[-(length % self.count):]
+
+        return quotient, remainder
